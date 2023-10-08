fa6b31c0ff3002592d228b512d088435392f37cf
UIT-20521205 <dinhiendung1026@gmail.com>
2023-10-08 11:51:27 +0700

    test

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -163,13 +163,41 @@ export class HomeComponent implements OnInit, OnDestroy {
 
     this.shareScreenPeer = new Peer('share_' + this.currentUser.userName, {
       config: {
-        'iceServers': [{
-          urls: this.configService.STUN_SERVER
-        },{
-          urls:this.configService.urlTurnServer,
-          username:this.configService.username,
-          credential:this.configService.password
-        }]
+        'iceServers': [
+          {
+            urls: "stun:stun.relay.metered.ca:80",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80?transport=tcp",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:80?transport=udp",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443?transport=tcp",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+          {
+            urls: "turn:a.relay.metered.ca:443?transport=udp",
+            username: "4af24cfab7a9e683a59be531",
+            credential: "N7WeALiaXC9Ti5i0",
+          },
+      ]
       }
     })
 
